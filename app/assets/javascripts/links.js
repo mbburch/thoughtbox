d484e17a5fcc89cd2ef3556aa83f42c77ed0b242
MB Burch <marybethburch@gmail.com>
2016-01-11 15:53:09 -0700

    add inline editing for url

--- app/assets/javascripts/links.js
+++ app/assets/javascripts/links.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   listenForRead();
   listenForUnread();
   editTitle();
+  editUrl();
 });
 
 function listenForRead() {
@@ -54,6 +55,34 @@ function editTitle() {
           success: function() {
             console.log("Success");
             $link.find('.title').text(linkParams.link.title);
+          },
+          error: function(xhr) {
+            console.log(xhr.responseText);
+          }
+        });
+      }
+    });
+  });
+}
+
+function editUrl() {
+  $('#all-links').on('click', '.url', function() {
+    var linkElement = event.toElement;
+    var urlElement = $(this).attr('contenteditable', 'true');
+    urlElement.focus();
+    urlElement.keypress(function() {
+      if (event.keyCode === 13) {
+        var $link = $(urlElement.parent().parent());
+        var $url = linkElement.textContent;
+        var linkParams = {link: { url: $url }};
+
+        $.ajax({
+          type: 'PUT',
+          url: '/api/v1/links/' + $link.attr('data-id'),
+          data: linkParams,
+          success: function() {
+            console.log("Success");
+            $link.find('.url').text(linkParams.link.url);
           },
           error: function(xhr) {
             console.log(xhr.responseText);
